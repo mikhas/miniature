61bf318b285ee62b680e877c20b63fe22046e3ac
Dariusz Mikulski <dariusz.mikulski@collabora.co.uk>
2010-01-22 17:26:44 +0100

    Add missing files

---
+++ src/telepathy/tpaccountmanager.cc
@@ -0,0 +1,83 @@
+/* Miniature - A chess board that goes always with you, ready to let
+ * you play and learn wherever you go.
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *              Dariusz Mikulski <dariusz.mikulski@collabora.co.uk>
+ *
+ *
+ * Miniature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Miniature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tpaccountmanager.h"
+
+#include <TelepathyQt4/Debug>
+#include <TelepathyQt4/Constants>
+#include <TelepathyQt4/Types>
+
+#include <TelepathyQt4/Account>
+#include <TelepathyQt4/AccountManager>
+#include <TelepathyQt4/PendingOperation>
+#include <TelepathyQt4/PendingReady>
+
+#include <QDebug>
+#include <QString>
+#include <QtGlobal>
+#include <QtMaemo5/QtMaemo5>
+
+namespace Miniature
+{
+
+TpAccountManager::TpAccountManager(QObject *parent)
+    : QObject(parent)
+{
+    Tp::registerTypes();
+    Tp::enableDebug(true);
+    Tp::enableWarnings(true);
+
+}
+
+TpAccountManager::~TpAccountManager()
+{
+}
+
+void TpAccountManager::onAMReady(Tp::PendingOperation *o)
+{
+    if(o->isError())
+    {
+        qDebug() << "Account Manager error";
+        return;
+    }
+
+    QList<Tp::AccountPtr> accountList = m_AM->validAccounts();
+
+    if(accountList.size() == 0)
+    {
+        QMaemo5InformationBox::information(NULL, QString("Can't find valid accounts!"));
+        return;
+    }
+}
+
+void TpAccountManager::chooseAccount()
+{
+     m_AM = Tp::AccountManager::create();
+
+     QObject::connect(m_AM->becomeReady(),
+            SIGNAL(finished(Tp::PendingOperation *)),
+            SLOT(onAMReady(Tp::PendingOperation *)));
+}
+
+#include "tpaccountmanager.moc.cc"
+
+};
+
