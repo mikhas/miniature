03d81d077bd0f290e6f0b025f758615dfd16a3c0
Michael Hasselmann <michael@taschenorakel.de>
2010-01-29 02:18:41 +0100

    Added a pre-game controller that handles the start screen

---
+++ src/pregame.cc
@@ -0,0 +1,153 @@
+/* Miniature - A chess board that goes always with you, ready to let
+ * you play and learn wherever you go.
+ *
+ * Copyright (C) 2010 Michael Hasselmann <michael@taschenorakel.de>
+ *
+ *
+ * Miniature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Miniature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pregame.h>
+#include <game.h>
+
+#include <QtCore>
+#include <QtGui>
+
+using namespace Miniature;
+
+MIconicButton::
+MIconicButton(const QPixmap &pixmap, const QString &label, QWidget *parent)
+: QWidget(parent),
+  m_pixmap(pixmap),
+  m_label(label)
+{
+    resize(sizeHint());
+    QSizePolicy fixed = QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    setSizePolicy(fixed);
+}
+
+MIconicButton::
+~MIconicButton()
+{}
+
+QSize MIconicButton::
+sizeHint() const
+{
+    return QSize(m_pixmap.width(), m_pixmap.height() + 30);
+}
+
+void MIconicButton::
+paintEvent (QPaintEvent *event)
+{
+    event->accept();
+    QPainter painter(this);
+
+    painter.drawPixmap(QRect(event->rect().x(),
+                             event->rect().y(),
+                             m_pixmap.width(),
+                             m_pixmap.height()),
+                       m_pixmap, m_pixmap.rect());
+
+    QFont font;
+    font.setPixelSize(20);
+    font.setWeight(QFont::Bold);
+    painter.setPen(QColor(Qt::white));
+    painter.drawText(QRect(event->rect().x(),
+                           event->rect().y() + m_pixmap.height(),
+                           m_pixmap.width(),
+                           30),
+                     Qt::AlignCenter, m_label);
+}
+
+void MIconicButton::
+mouseReleaseEvent(QMouseEvent *event)
+{
+    event->accept();
+    Q_EMIT pressed();
+}
+
+MPreGame::
+MPreGame(QObject *parent)
+: QObject(parent),
+  m_main_window(new MMainWindow)
+{
+    m_main_window->registerActions();
+    m_main_window->connectActions();
+}
+
+MPreGame::
+~MPreGame()
+{}
+
+void MPreGame::
+onStartScreenRequested()
+{
+    QWidget *central = new QWidget;
+    QVBoxLayout *vbox = new QVBoxLayout;
+    central->setLayout(vbox);
+    vbox->setAlignment(Qt::AlignCenter);
+
+    QWidget *buttons = new QWidget;
+    vbox->addWidget(buttons);
+
+    QHBoxLayout *hbox = new QHBoxLayout;
+    buttons->setLayout(hbox);
+
+    MIconicButton *new_game = new MIconicButton(QPixmap("/usr/share/themes/alpha/backgrounds/app_install_games.png"),
+                                                tr("Local Game"), m_main_window);
+    hbox->addWidget(new_game);
+
+    MIconicButton *new_p2p_game = new MIconicButton(QPixmap("/usr/share/themes/alpha/backgrounds/app_install_network.png"),
+                                                    QString("P2P Game"), m_main_window);
+    hbox->addWidget(new_p2p_game);
+
+    MMainWindow::setupPreGameUi(m_main_window, buttons);
+    m_main_window->show();
+
+    connect(new_game, SIGNAL(pressed()),
+            this, SLOT(onNewGameRequested()));
+
+    connect(new_game, SIGNAL(pressed()),
+            this, SLOT(onNewP2PGameRequested()));
+}
+
+void MPreGame::
+onNewGameRequested()
+{
+    QMainWindow *window = new QMainWindow(m_main_window);
+
+    MBoardView *view = new MBoardView(window);
+    MGame *game = new Miniature::MGame(view, window);
+    game->newGame();
+
+    MMainWindow::setupGameUi(window, view);
+    window->show();
+}
+
+void MPreGame::
+onNewP2PGameRequested()
+{
+    QMainWindow *window = new QMainWindow(m_main_window);
+
+    MBoardView *view = new MBoardView(window);
+
+    QMessageBox::critical(window, tr("Not implemented yet!"), tr("Sorry, but this feature is currently missing ..."));
+
+    //TODO: Activate me!
+    //MGame *game = new Miniature::MGame(view, window);
+    //game->newP2PGame();
+
+    MMainWindow::setupGameUi(window, view);
+    window->show();
+}
---
+++ src/pregame.h
@@ -0,0 +1,88 @@
+/* Miniature - A chess board that goes always with you, ready to let
+ * you play and learn wherever you go.
+ *
+ * Copyright (C) 2010 Michael Hasselmann <michael@taschenorakel.de>
+ *
+ *
+ * Miniature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Miniature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <main.h>
+
+namespace Miniature {
+
+/*!
+ *  A helper class to create clickable images. Using pixmaps on push buttons
+ *  doesn't quite yield the desired effect.
+ */
+class MIconicButton
+: public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MIconicButton(const QPixmap &pixmap, const QString &label, QWidget *parent = 0);
+    virtual ~MIconicButton();
+    virtual QSize sizeHint() const;
+
+Q_SIGNALS:
+    void pressed();
+
+protected:
+    virtual void paintEvent(QPaintEvent *event);
+    virtual void mouseReleaseEvent(QMouseEvent *event);
+
+private:
+    QPixmap m_pixmap;
+    QString m_label;
+};
+
+/*!
+ *  This class controls the start screen and forwards control to the correct
+ *  MGame controller once the user made a choice
+ */
+class MPreGame
+: public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MPreGame(QObject *parent = 0);
+    virtual ~MPreGame();
+
+public Q_SLOTS:
+    /*!
+     *  Shows the start screen where the user can choose one of the available game modi.
+     *  In the near future, this method will also initialize the prerendering
+     *  of all the needed QImages in a separate thread.
+     *  @param[in] view the view where we place the start screen into.
+     */
+    void onStartScreenRequested();
+
+    /*!
+     *  Starts a game in "pub mode"
+     */
+    void onNewGameRequested();
+
+    /*!
+     *  Starts a new P2P game.
+     */
+    void onNewP2PGameRequested();
+
+private:
+  MMainWindow *m_main_window;
+
+};
+
+} // namespace Miniature
