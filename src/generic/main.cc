ad16651e8e80721594e77994283bb996c8ea5277
Dariusz Mikulski <dariusz.mikulski@collabora.co.uk>
2010-03-04 18:17:54 +0100

    Fix portability

--- src/generic/main.cc
+++ src/generic/main.cc
@@ -18,9 +18,8 @@
  * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <config.h>
-#include <main.h>
-#include <about.ui.h>
+#include "../config.h"
+#include "../main.h"
 
 /*! \brief Container for Miniature main actions.
  */
@@ -31,21 +30,15 @@ public:
      *  @param[in] owner The owner of the contained actions.
      */
     explicit mActions(QObject *owner)
-    : new_game(new QAction(owner)),
-      next_move(new QAction(owner)),
-      prev_move(new QAction(owner)),
-      show_about_dialog(new QAction(owner)),
-      toggle_rotation(new QAction(owner))
+      : show_about_dialog(new QAction(owner)),
+      show_game_log(new QAction(owner))
     {}
 
     virtual ~mActions()
     {}
 
-    QAction* new_game;
-    QAction* next_move;
-    QAction* prev_move;
     QAction* show_about_dialog;
-    QAction* toggle_rotation;
+    QAction* show_game_log;
 };
 
 /*! \brief Container for Miniature dialogs.
@@ -58,43 +51,78 @@ public:
 
     virtual ~mDialogs()
     {}
-
-    Ui::AboutDialog about_dialog;
 };
 
-MMainWindow::MMainWindow()
-: QMainWindow(),
-  m_game(0),
-  m_view(0),
-  m_actions(new mActions(this))
+const char* const dbus_service_name = "org.maemo.miniature";
+const char* const dbus_service_path = "/org/maemo/miniature";
+
+MMainWindow::MMainWindow(MGameLog *log, QWidget *parent)
+: QMainWindow(parent),
+  m_log(log),
+  m_actions(new mActions(this)),
+  m_session(QDBusConnection::sessionBus())
 {
-    setup();
-    setupUi();
+    if(!m_session.registerService(dbus_service_name))
+    {
+        qWarning("Cannot register D-Bus service. Trying to activate other instance.");
+        m_session.call(QDBusMessage::createMethodCall(dbus_service_name, dbus_service_path,
+            dbus_service_name, "top_application"));
+        exit(EXIT_FAILURE);
+    }
+
+    if(!m_session.registerObject(dbus_service_path, this))
+    {
+        qFatal("Cannot register D-Bus service. Aborting.");
+        exit(EXIT_FAILURE);
+    }
+
+    new QDBusAppActivator(this);
+
     registerActions();
     connectActions();
-
-    m_actions->new_game->trigger();
 }
 
-void MMainWindow::setupUi()
+void MMainWindow::setupPreGameUi(QMainWindow *window, QWidget *subview)
 {
-    setWindowTitle(tr("Miniature"));
-    resize(480, 800);
+    window->setWindowTitle(tr("Miniature"));
+    window->resize(800, 480);
 
-    QWidget *central = new QWidget(this);
-    setCentralWidget(central);
+#if defined(Q_WS_MAEMO_5)
+    window->setAttribute(Qt::WA_Maemo5StackedWindow);
+    window->setAttribute(Qt::WA_Maemo5LandscapeOrientation, true);
+#endif  
+
+    window->setCentralWidget(subview);
+}
+
+void MMainWindow::setupGameUi(QMainWindow *window, QGraphicsView *subview)
+{
+    window->setWindowTitle(tr("Miniature"));
+    window->resize(480, 800);
+
+    window->setAttribute(Qt::WA_DeleteOnClose);
+
+#if defined(Q_WS_MAEMO_5)
+    window->setAttribute(Qt::WA_Maemo5StackedWindow);
+    window->setAttribute(Qt::WA_Maemo5PortraitOrientation, true);
+#endif
+
+    window->setWindowState(window->windowState() | Qt::WindowFullScreen);
+
+    QWidget *central = new QWidget(window);
+    window->setCentralWidget(central);
 
     QVBoxLayout *vbox = new QVBoxLayout(central);
     vbox->setContentsMargins(0, 0, 0, 0);
 
     QSizePolicy v_expand(QSizePolicy::Preferred, QSizePolicy::Expanding);
-    m_view->setSizePolicy(v_expand);
-    m_view->setMinimumSize(QSize(480, 650));
-    m_view->setFrameShape(QFrame::NoFrame);
-    m_view->setLineWidth(0);
-    m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    vbox->addWidget(m_view);
+    subview->setSizePolicy(v_expand);
+    subview->setMinimumSize(QSize(480, 800));
+    subview->setFrameShape(QFrame::NoFrame);
+    subview->setLineWidth(0);
+    subview->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    subview->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    vbox->addWidget(subview);
 
     QSpacerItem *v_spacer = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
     vbox->addItem(v_spacer);
@@ -102,65 +130,87 @@ void MMainWindow::setupUi()
 
 void MMainWindow::registerActions()
 {
-    m_actions->new_game->setText(tr("New Game"));
-    m_actions->new_game->setShortcut(tr("Ctrl+N"));
-
-    m_actions->next_move->setText(tr("Next Move"));
-    m_actions->next_move->setShortcut(tr("Right"));
-
-    m_actions->prev_move->setText(tr("Previous Move"));
-    m_actions->prev_move->setShortcut(tr("Left"));
-
-    m_actions->toggle_rotation->setText(tr("Toggle Rotation"));
-    m_actions->toggle_rotation->setShortcut(tr("Ctrl+R"));
-
     m_actions->show_about_dialog->setText(tr("About"));
     m_actions->show_about_dialog->setShortcut(tr("Ctrl+?"));
+
+    m_actions->show_game_log->setText(tr("Show Game Log"));
+    m_actions->show_game_log->setShortcut(tr("Ctrl+L"));
 
     QMenuBar *menu_bar = new QMenuBar(this);
     menu_bar->setGeometry(QRect(0, 0, 480, 25));
 
-    QMenu *menu = new QMenu(menu_bar);
-    menu_bar->addAction(menu->menuAction());
-    menu->addAction(m_actions->new_game);
-    menu->addAction(m_actions->next_move);
-    menu->addAction(m_actions->prev_move);
-    menu->addAction(m_actions->toggle_rotation);
-
-    QMenu *help = new QMenu(menu_bar);
-    menu_bar->addAction(help->menuAction());
-    help->addAction(m_actions->show_about_dialog);
+    QMenu *main = new QMenu(menu_bar);
+    menu_bar->addAction(main->menuAction());
+    main->addAction(m_actions->show_game_log);
+    main->addAction(m_actions->show_about_dialog);
 
     setMenuBar(menu_bar);
 }
 
 void MMainWindow::connectActions()
 {
-    // Connect actions with MGame controller.
-    connect(m_actions->new_game, SIGNAL(triggered()),
-            m_game, SLOT(newGame()));
-    connect(m_actions->next_move, SIGNAL(triggered()),
-            m_game, SLOT(nextMove()));
-    connect(m_actions->prev_move, SIGNAL(triggered()),
-            m_game, SLOT(prevMove()));
-    connect(m_actions->toggle_rotation, SIGNAL(triggered()),
-            m_game, SIGNAL(togglePieceRotations()));
+    QSignalMapper *mapper = new QSignalMapper(this);
+    mapper->setMapping(m_actions->show_game_log, this);
 
-    // Connect action to show about dialog
-    // TODO: find out why the QDialog crashes in sbox! gdb reports instances of
-    // QDialog as struct*, so it is probably a linker issue.
+    connect(m_actions->show_game_log, SIGNAL(triggered()),
+            mapper,                   SLOT(map()));
+
+    connect(mapper, SIGNAL(mapped(QWidget *)),
+            m_log,  SLOT(showLog(QWidget *)));
+
     connect(m_actions->show_about_dialog, SIGNAL(triggered()),
-            this, SLOT(showAboutDialog()));
+            this,                         SLOT(showAboutDialog()));
 }
 
 void MMainWindow::showAboutDialog()
 {
     QFont font = QFont("helvetica", 14, QFont::Normal);
-    QDialog dialog;
-    m_dialogs->about_dialog.setupUi(&dialog);
-    m_dialogs->about_dialog.slogan->setFont(font);
-    m_dialogs->about_dialog.version->setFont(font);
-    m_dialogs->about_dialog.version->setText(QString("%1: %2").arg(tr("Version"))
-                                                              .arg(PACKAGE_VERSION));
-    dialog.exec();
+    QDialog *dialog = new QDialog(QApplication::activeWindow());
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->setWindowTitle(tr("About Miniature"));
+    dialog->resize(-1, 480);
+
+    QSizePolicy policy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    policy.setHorizontalStretch(0);
+    policy.setVerticalStretch(1);
+
+    QVBoxLayout *vbox = new QVBoxLayout(dialog);
+
+    QLabel *slogan = new QLabel(dialog);
+    slogan->setText(tr("A chess board that always goes with you."));
+    slogan->setAlignment(Qt::AlignCenter);
+    vbox->addWidget(slogan);
+
+    // TODO: Find correct color roles that are used in the QTextBrowser, and remove the HTML style hack.
+    QPalette lightBg;
+    lightBg.setColor(QPalette::Base, QColor(Qt::white));
+    lightBg.setColor(QPalette::WindowText, QColor(Qt::black));
+
+    QTextBrowser *description = new QTextBrowser(dialog);
+    description->setPalette(lightBg);
+    description->setSizePolicy(policy);
+
+    description->setTextInteractionFlags(Qt::LinksAccessibleByKeyboard|Qt::LinksAccessibleByMouse|Qt::TextBrowserInteraction|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+    description->setOpenExternalLinks(true);
+    description->setText(tr(
+"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head/><body style=\"color:black; background:white;\">\n"
+"<div style=\"margin-bottom:1.5em;\">Miniature version: %1</div>\n"
+"<div style=\"margin-bottom:1.5em;\">Check the <a href=\"http://wiki.maemo.org/Miniature\">website</a> for more information,\n"
+"feedback and ways to get involved. Miniature is free software for you to enjoy\n"
+"(<a href=\"http://creativecommons.org/licenses/GPL/2.0/\">GPL2 or later</a>).\n"
+"<div>&#169; <a href=\"http://wiki.maemo.org/Miniature#Contributors\">Contributors</a>.</div>\n"
+"</body></html>"
+        ).arg(PACKAGE_VERSION));
+
+    vbox->addWidget(description);
+
+    QDialogButtonBox *buttons = new QDialogButtonBox(dialog);
+    buttons->setOrientation(Qt::Horizontal);
+    buttons->setStandardButtons(QDialogButtonBox::Close);
+    vbox->addWidget(buttons);
+
+    dialog->show();
 }
+
