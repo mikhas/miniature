2424d8266369457cf60a8e7a4fbee4cad5af781b
Michael Hasselmann <michael@taschenorakel.de>
2010-04-08 02:01:01 +0300

    Move non-UI related game parts into MGameStore

---
+++ src/game_store.cc
@@ -0,0 +1,301 @@
+/* Miniature - A chess board that goes always with you, ready to let
+ * you play and learn wherever you go.
+ *
+ * Copyright (C) 2010 Michael Hasselmann <michael@taschenorakel.de>
+ *
+ *
+ * Miniature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Miniature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <game_store.h>
+
+using namespace Miniature;
+
+MGameStore::
+MGameStore(QObject *parent)
+: QObject(parent),
+  m_candidate_move(MPosition()),
+  m_index(-1)
+{}
+
+MGameStore::
+~MGameStore()
+{}
+
+void MGameStore::
+reset(const MPosition &start_position)
+{
+    m_game.clear();
+    m_game.append(start_position);
+
+    m_candidate_move = mHalfMove(start_position);
+    m_index = 0;
+
+    Q_EMIT (hasWhiteToMove() ? whiteToMove(start_position)
+                             : blackToMove(start_position));
+}
+
+int MGameStore::
+getIndex() const
+{
+    return m_index;
+}
+
+void MGameStore::
+setupStartPosition()
+{
+    m_game.clear();
+    m_game = MPositionList(); // leaky?
+    Q_ASSERT(m_game.empty());
+
+    //Q_CHECK_PTR(m_view);
+    //MDashboardItem *top = m_view->getTopDashboardItem();
+    //MDashboardItem *bottom = m_view->getBottomDashboardItem();
+
+    //top->resetUi();
+    //bottom->resetUi();
+
+    //top->disableConfirmButton();
+    //bottom->disableConfirmButton();
+
+    //top->disableRequestsButton();
+    //bottom->enableRequestsButton();
+
+    MPosition pos;
+
+    pos.addPieceAt(new MRook(MPiece::BLACK), QPoint(0,0));
+    pos.addPieceAt(new MRook(MPiece::BLACK), QPoint(7,0));
+    pos.addPieceAt(new MRook(MPiece::WHITE), QPoint(0,7));
+    pos.addPieceAt(new MRook(MPiece::WHITE), QPoint(7,7));
+
+    pos.addPieceAt(new MKnight(MPiece::BLACK), QPoint(1,0));
+    pos.addPieceAt(new MKnight(MPiece::BLACK), QPoint(6,0));
+    pos.addPieceAt(new MKnight(MPiece::WHITE), QPoint(1,7));
+    pos.addPieceAt(new MKnight(MPiece::WHITE), QPoint(6,7));
+
+    pos.addPieceAt(new MBishop(MPiece::BLACK), QPoint(2,0));
+    pos.addPieceAt(new MBishop(MPiece::BLACK), QPoint(5,0));
+    pos.addPieceAt(new MBishop(MPiece::WHITE), QPoint(2,7));
+    pos.addPieceAt(new MBishop(MPiece::WHITE), QPoint(5,7));
+
+    pos.addPieceAt(new MQueen(MPiece::BLACK), QPoint(3,0));
+    pos.addPieceAt(new MQueen(MPiece::WHITE), QPoint(3,7));
+
+    pos.addPieceAt(new MKing(MPiece::BLACK), QPoint(4,0));
+    pos.addPieceAt(new MKing(MPiece::WHITE), QPoint(4,7));
+
+    for (int i = 0; i < 8; ++i)
+    {
+        pos.addPieceAt(new MPawn(MPiece::BLACK), QPoint(i,1));
+        pos.addPieceAt(new MPawn(MPiece::WHITE), QPoint(i,6));
+    }
+
+    pos.resetCastling();
+
+    m_game.append(pos);
+    m_index = 0;
+    m_candidate_move = mHalfMove(pos);
+
+    Q_ASSERT(!m_game.empty());
+    Q_EMIT whiteToMove(pos);
+}
+
+bool MGameStore::
+hasWhiteToMove() const
+{
+    return (isValidPosition(m_index) && (MPiece::WHITE == (m_game[m_index]).getColourToMove()));
+}
+
+bool MGameStore::
+hasBlackToMove() const
+{
+    return !hasWhiteToMove();
+}
+
+void MGameStore::
+onPositionRequested(int index)
+{
+    if (!isValidPosition(index))
+        return;
+
+    m_index = index;
+    MPosition pos = m_game[m_index];
+
+    //m_board_item->hidePieces();
+    pos.updatePieces();
+    m_candidate_move.deSelect();
+    m_candidate_move = mHalfMove(pos);
+    Q_EMIT (hasWhiteToMove() ? whiteToMove(pos)
+                             : blackToMove(pos));
+    //updatePlayerStatus(pos);
+
+    //MDashboardItem *top = m_view->getTopDashboardItem();
+    //MDashboardItem *bottom = m_view->getBottomDashboardItem();
+    //top->resetUi();
+    //bottom->resetUi();
+
+    //top->disableConfirmButton();
+    //bottom->disableConfirmButton();
+
+/*
+    if (isTurnOfTopPlayer())
+    {
+        top->enableRequestsButton();
+        bottom->disableRequestsButton();
+    }
+    else
+    {
+        top->disableRequestsButton();
+        bottom->enableRequestsButton();
+    }
+*/
+}
+
+void MGameStore::
+onCandidateMoveConfirmed()
+{
+/*
+    MDashboardItem *top = m_view->getTopDashboardItem();
+    MDashboardItem *bottom = m_view->getBottomDashboardItem();
+
+    top->resetUi();
+    bottom->resetUi();
+
+    top->disableConfirmButton();
+    bottom->disableConfirmButton();
+*/
+
+    // Assumption: The move is already visible on the board, hence we only add
+    // the transitional position to the position list.
+    MPosition pos = MPosition(m_candidate_move.getPosition());
+    m_candidate_move.deSelect();
+    m_candidate_move = mHalfMove(pos);
+    m_game.insert(++m_index, pos);
+
+    Q_EMIT (hasWhiteToMove() ? whiteToMove(pos)
+                             : blackToMove(pos));
+
+/*
+    updatePlayerStatus(position);
+
+    if (isTurnOfTopPlayer())
+    {
+        top->enableRequestsButton();
+        bottom->disableRequestsButton();
+        Q_EMIT turnOfTopPlayer();
+    }
+    else
+    {
+        top->disableRequestsButton();
+        bottom->enableRequestsButton();
+        Q_EMIT turnOfBottomPlayer();
+    }
+*/
+}
+
+void MGameStore::
+onPieceSelected(MPiece *piece)
+{
+    // If invalid piece was selected => reset selection, purple flashing.
+    // Currently, most piece selections are "valid" (because the logic analyzer
+    // doesnt know how to handle that yet)
+    Q_ASSERT(isValidPosition(m_index));
+
+    // Only allow selection if piece has the correct colour.
+    MPosition pos = m_game[m_index];
+    if (piece && piece->getColour() == pos.getColourToMove())
+    {
+        const bool was_already_selected = m_candidate_move.isSelected(piece);
+        m_candidate_move.undo();
+        m_candidate_move.deSelect();
+
+/*
+        MDashboardItem *top = m_view->getTopDashboardItem();
+        MDashboardItem *bottom = m_view->getBottomDashboardItem();
+
+        top->resetUi();
+        bottom->resetUi();
+
+        top->disableConfirmButton();
+        bottom->disableConfirmButton();
+*/
+
+        // Don't re-select the same piece - see b.m.o bug #7868.
+        if (!was_already_selected)
+        {
+            m_candidate_move = mHalfMove(pos);
+            m_candidate_move.select(piece->mapToCell());
+        }
+
+        Q_EMIT candidatePieceSelected();
+    }
+    // Must be an attempt to capture an enemy piece. Let's forward it then!
+    // Note: From my current understanding, this is the only case where
+    // onTargetSelected is called when target points to a piece. Usually,
+    // onTargetSelected is always only called when target points to an empty
+    // cell.
+    else if (piece && m_candidate_move.isValid())
+    {
+        onTargetSelected(piece->mapToCell());
+    }
+
+    // TODO: complain here
+}
+
+void MGameStore::
+onTargetSelected(const QPoint &target)
+{
+    // Ignores invalid mouse clicks, updates/reset dashboard items when move was
+    // valid/invalid.
+
+    //MDashboardItem *top = m_view->getTopDashboardItem();
+    //MDashboardItem *bottom = m_view->getBottomDashboardItem();
+
+    //top->resetUi();
+    //bottom->resetUi();
+
+    if (m_candidate_move.isUndoRequest(target))
+    {
+        m_candidate_move.undo(); // but do not deselect!
+        Q_EMIT moveDiscarded();
+        //top->disableConfirmButton();
+        //bottom->disableConfirmButton();
+    }
+    else if (m_candidate_move.applyToTarget(target))
+    {
+        Q_EMIT moveConfirmationRequested();
+        /*
+        if (isTurnOfBottomPlayer())
+            bottom->enableConfirmButton();
+        else
+            top->enableConfirmButton();
+        */
+    }
+    else
+    {
+        Q_EMIT invalidTargetSelected();
+        /*
+        if (isTurnOfBottomPlayer())
+            bottom->flash();
+        else
+            top->flash();
+        */
+    }
+}
+
+bool MGameStore::
+isValidPosition(int index) const
+{
+    return (!m_game.empty() && -1 < index && index < m_game.size());
+}
---
+++ src/game_store.h
@@ -0,0 +1,114 @@
+/* Miniature - A chess board that goes always with you, ready to let
+ * you play and learn wherever you go.
+ *
+ * Copyright (C) 2010 Michael Hasselmann <michael@taschenorakel.de>
+ *
+ *
+ * Miniature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Miniature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Miniature. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GAME_STORE_H__
+#define GAME_STORE_H__
+
+#include <half_move.h>
+#include <position.h>
+#include <QtCore>
+
+namespace Miniature
+{
+
+/*! This class stores the half moves of a game (and maybe even *games*, at a later point) and
+ *  provides signals for position changes (e.g., when navigating through a game or setting up a
+ *  position). It is unaware of the UI.
+ */
+class MGameStore
+: public QObject
+{
+    Q_OBJECT
+
+public:
+    typedef QList<MPosition> MPositionList;
+
+    explicit MGameStore(QObject *parent = 0);
+    virtual ~MGameStore();
+
+    //! Empties game store and resets to \a start_position.
+    void reset(const MPosition &start_position);
+
+    //! Returns the index of the current position.
+    int getIndex() const;
+
+    /*!
+     *  Creates a MPosition with the default chess start position and stores it
+     *  in m_game.
+     */
+    void setupStartPosition();
+
+    //! Returns whose turn it is.
+    bool hasWhiteToMove() const;
+
+    //! \sa hasWhiteToMove - added for completeness wrt. signals.
+    bool hasBlackToMove() const;
+
+Q_SIGNALS:
+    void whiteToMove(const MPosition &position);
+    void blackToMove(const MPosition &position);
+
+    void candidatePieceSelected();
+    void moveDiscarded();
+    void moveConfirmationRequested();
+    void invalidTargetSelected();
+
+public Q_SLOTS:
+    //! Sets position to index and emits {white,black}ToMove.
+    void onPositionRequested(int index);
+
+    /*!
+     *  Adds the selected candidate move to the store and emits {white,
+     *  black}ToMove.
+     */
+    void onCandidateMoveConfirmed();
+
+    /*!
+     *  Evaluates the selected piece and emits candidatePieceSelected,
+     *  moveDiscarded, moveConfirmationRequested, invalidTargetSelected.
+     *  @param[in] piece The selected piece.
+     */
+    void onPieceSelected(MPiece *piece);
+
+    /*!
+     *  Evaluates the selected target and emits moveDiscarded,
+     *  moveConfirmationRequested, invalidTargetSelected.
+     *  @param[in] target The selected target cell.
+     */
+    void onTargetSelected(const QPoint &target);
+
+private:
+    /*!
+     *  \brief Returns whether the position exists in m_game.
+     *  @param[in] half_move_index The index of the queried position.
+     */
+    bool isValidPosition(int half_move_index) const;
+
+    MPositionList m_game; /*!< Stores the game history as a list of positions. >*/
+    mHalfMove m_candidate_move; /*!< Encapsulates all transitional data
+        for a potential half move. Remember that our UI requires a player to
+        confirm moves so the UI needs to be able to show candidate moves that
+        will never be part of the game history. >*/
+    int m_index; /*!< An index to the positions stored in m_game. >*/
+};
+
+}
+
+#endif // GAME_STORE_H__
